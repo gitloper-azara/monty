2e0a1283f20c9720dbb564264ca0c117f930f909
azara <yyushahu02@gmail.com>
2023-12-14 14:49:52 +0000

    handle div operation

--- 1_opcodes.c
+++ 1_opcodes.c
@@ -42,11 +42,37 @@ void op_sub(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !(*stack) || !((*stack)->next))
 	{
-		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n",
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n",
 			line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	(*stack)->next->n -= (*stack)->n;
+	op_pop(stack, line_number);
+}
+
+/**
+ * op_div - a function that divides the second top element of the stack by the
+ * top element of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the monty file
+ */
+
+void op_div(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
 	op_pop(stack, line_number);
 }
